8f754099ba0e2a836cee7daf462bdda3acde3c7a
kokchun <kokchungiang@gmail.com>
2025-09-09 11:25:10 +0200

    create book endpoint

--- code-alongs/08_fastapi_crud/api.py
+++ code-alongs/08_fastapi_crud/api.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from data_processing import library_data
+from data_processing import library_data, Book
 
 app = FastAPI()
 
@@ -10,3 +10,16 @@ books = library.books
 @app.get("/books")
 async def read_books():
     return books
+
+# path parameter
+@app.get("/books/title/{title}")
+async def read_book_by_title(title: str):
+    return [book for book in books if book.title.casefold() == title.casefold()]
+
+
+@app.post("/books/create_book")
+async def create_book(book_request: Book):
+    new_book = Book.model_validate(book_request)
+    books.append(new_book)
+
+    return new_book
