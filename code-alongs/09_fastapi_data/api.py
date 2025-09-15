12bbe992a2b46371467b64e985db2853da590da0
kokchun <kokchungiang@gmail.com>
2025-09-15 13:07:44 +0200

    fixed a bug with data_explorer mutation

--- code-alongs/09_fastapi_data/api.py
+++ code-alongs/09_fastapi_data/api.py
@@ -1,26 +1,31 @@
 from fastapi import FastAPI
 from data_processing import DataExplorer
 
-data_explorer = DataExplorer()
 
 app = FastAPI()
+
 
 @app.get("/api/sales")
 async def read_sales():
     # implement this code to return json data in this endpoint
 
+    data_explorer = DataExplorer()
     return data_explorer.json_response()
+
 
 @app.get("/api/sales/summary")
 async def read_summary_data():
     """shows summary statistics"""
+    data_explorer = DataExplorer()
     return data_explorer.summary().json_response()
 
 
 @app.get("/api/sales/kpis")
 async def read_kpis_by_country(country: str):
     """KPIs based on country"""
+    data_explorer = DataExplorer()
     return data_explorer.kpis(country=country)
+
 
 # to run the API
 # uvicorn api:app --reload
