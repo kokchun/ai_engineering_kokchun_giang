fbe9c730c91de929dfb637856fb8660e1899e57d
kokchun <kokchungiang@gmail.com>
2025-09-15 10:29:52 +0200

    json response

--- code-alongs/09_fastapi_data/data_processing.py
+++ code-alongs/09_fastapi_data/data_processing.py
@@ -8,8 +8,16 @@ class DataExplorer:
     def __init__(self, limit = 100):
         self._df = df.head(limit)
 
+    @property
+    def df(self):
+        return self._df
+    
+
+    def json_response(self):
+        json_data = self.df.to_json(orient = "records")
+        return json_data
 
 if __name__ == "__main__":
     data_explorer = DataExplorer()
 
-    print(data_explorer._df)
+    print(data_explorer.json_response())
