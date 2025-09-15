c71716da1645e04335e599b8cd1a6d88e7b0c42f
kokchun <kokchungiang@gmail.com>
2025-09-15 13:11:37 +0200

    apirouter

--- code-alongs/09_fastapi_data/api.py
+++ code-alongs/09_fastapi_data/api.py
@@ -1,11 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 from data_processing import DataExplorer
 
 
 app = FastAPI()
 
+router = APIRouter(prefix="/api/sales")
 
-@app.get("/api/sales")
+
+@router.get("")
 async def read_sales():
     # implement this code to return json data in this endpoint
 
@@ -13,19 +15,21 @@ async def read_sales():
     return data_explorer.json_response()
 
 
-@app.get("/api/sales/summary")
+@router.get("/summary")
 async def read_summary_data():
     """shows summary statistics"""
     data_explorer = DataExplorer()
     return data_explorer.summary().json_response()
 
 
-@app.get("/api/sales/kpis")
+@router.get("/kpis")
 async def read_kpis_by_country(country: str):
     """KPIs based on country"""
     data_explorer = DataExplorer()
     return data_explorer.kpis(country=country)
 
+
+app.include_router(router)
 
 # to run the API
 # uvicorn api:app --reload
