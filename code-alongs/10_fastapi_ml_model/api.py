8d3d34b8fb0444f0cac7a784b8e11849e6496859
kokchun <kokchungiang@gmail.com>
2025-09-23 10:44:33 +0200

    post prediction started

--- code-alongs/10_fastapi_ml_model/api.py
+++ code-alongs/10_fastapi_ml_model/api.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from data_processing import IrisData
+from data_processing import IrisData, IrisInput, PredictionOutput
+import pandas as pd 
 
 app = FastAPI()
 
@@ -8,3 +9,7 @@ iris = IrisData()
 @app.get("/api/")
 def read_data():
     return iris.to_json()
+
+@app.post("/api/predict", response_model=PredictionOutput)
+def predict_flower(payload: IrisInput):
+    data_to_predict = pd.DataFrame(payload.model_dump())
--- code-alongs/10_fastapi_ml_model/data_processing.py
+++ code-alongs/10_fastapi_ml_model/data_processing.py
@@ -1,5 +1,6 @@
 import pandas as pd 
 from constants import DATA_PATH
+from pydantic import BaseModel, Field
 
 df = pd.read_csv(DATA_PATH / "Iris.csv", index_col=0)
 
@@ -11,6 +12,18 @@ class IrisData:
     def to_json(self):
         return self.df.to_dict(orient="records")
     
+# request/response schemas
+class IrisInput(BaseModel):
+    SepalLengthCm: float = Field()
+    SepalWidthCm: float = Field()
+    PetalLengthCm: float = Field()
+    PetalWidthCm: float = Field()
+
+class PredictionOutput(BaseModel):
+    predicted_flower: str
+
+
+
 if __name__ == "__main__":
     iris = IrisData()
     print(iris.to_json())
