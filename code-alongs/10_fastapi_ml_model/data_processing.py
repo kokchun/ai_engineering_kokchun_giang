19498de6da012f179bf0b728a25030b6097dd8cf
kokchun <kokchungiang@gmail.com>
2025-09-30 09:35:28 +0200

    IrisInput fields specified

--- code-alongs/10_fastapi_ml_model/data_processing.py
+++ code-alongs/10_fastapi_ml_model/data_processing.py
@@ -1,4 +1,4 @@
-import pandas as pd 
+import pandas as pd
 from constants import DATA_PATH
 from pydantic import BaseModel, Field
 
@@ -11,17 +11,18 @@ class IrisData:
 
     def to_json(self):
         return self.df.to_dict(orient="records")
-    
+
+
 # request/response schemas
 class IrisInput(BaseModel):
-    SepalLengthCm: float = Field()
-    SepalWidthCm: float = Field()
-    PetalLengthCm: float = Field()
-    PetalWidthCm: float = Field()
+    SepalLengthCm: float = Field(6, gt=4, lt=8.2)
+    SepalWidthCm: float = Field(3, gt=1.7, lt=4.8)
+    PetalLengthCm: float = Field(3.8, gt=0.8, lt=7.1)
+    PetalWidthCm: float = Field(1.2, gt=0.05, lt=2.7)
+
 
 class PredictionOutput(BaseModel):
     predicted_flower: str
-
 
 
 if __name__ == "__main__":
