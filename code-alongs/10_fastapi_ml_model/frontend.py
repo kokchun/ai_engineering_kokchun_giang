07bbe9d7cd6051595c25854cbb519b057f09f976
kokchun <kokchungiang@gmail.com>
2025-09-30 11:43:27 +0200

    frontend predicted flower

--- code-alongs/10_fastapi_ml_model/frontend.py
+++ code-alongs/10_fastapi_ml_model/frontend.py
@@ -5,16 +5,44 @@ import pandas as pd
 iris_data = read_api_endpoint("/api")
 df = pd.DataFrame(iris_data.json())
 
+# TODO: try to predict a flower using post_api_endpoint and
+# write out the result in streamlit
+
+
 
 def layout():
     st.markdown("# Classify your iris flower")
 
     with st.form("iris_data"):
         sepal_length = st.number_input(
-            "Sepal length (cm)", min_value=4.01, max_value=8.49, value=6.0
+            "Sepal length (cm)", min_value=4.01, max_value=8.49, value=6.0, step=0.1
+        )
+        sepal_width = st.number_input(
+            "Sepal width (cm)", min_value=1.81, max_value=4.99, value=2.5, step=0.1
+        )
+        petal_length = st.number_input(
+            "Petal length (cm)", min_value=0.81, max_value=7.49, value=4.5, step=0.1
+        )
+        petal_width = st.number_input(
+            "Petal width (cm)", min_value=0.01, max_value=2.99, value=1.2, step=0.1
         )
 
         submitted = st.form_submit_button("PREDICT FLOWER")
+
+    if submitted:
+        payload = {
+            "SepalLengthCm": sepal_length,
+            "SepalWidthCm": sepal_width,
+            "PetalLengthCm": petal_length,
+            "PetalWidthCm": petal_width,
+        }
+
+        response = post_api_endpoint(payload, endpoint="/api/predict")
+        predicted_flower = response.json().get("predicted_flower")
+
+
+        st.markdown(f"Predicted flower: {predicted_flower}")
+
 
     print(f"{sepal_length = }")
     print(f"{submitted = }")
