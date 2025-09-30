8a71df1575b8dccd5a4033f0b69bb0a9e2a5d6d3
kokchun <kokchungiang@gmail.com>
2025-09-30 10:17:06 +0200

    testted out the endpoints

--- code-alongs/10_fastapi_ml_model/helpers.py
+++ code-alongs/10_fastapi_ml_model/helpers.py
@@ -1,13 +1,28 @@
-import requests 
+import requests
 from urllib.parse import urljoin
 from pprint import pprint
 
-def read_api_endpoint(endpoint = "/", base_url = "http://127.0.0.1:8000"):
+
+def read_api_endpoint(endpoint="/", base_url="http://127.0.0.1:8000"):
     url = urljoin(base_url, endpoint)
     response = requests.get(url)
-    
+
     return response
 
 
-if __name__ == '__main__':
-    pprint(read_api_endpoint("/api").json())
+def post_api_endpoint(payload, endpoint="/", base_url="http://127.0.0.1:8000"):
+    url = urljoin(base_url, endpoint)
+    response = requests.post(url=url, json=payload)
+
+    return response
+
+
+if __name__ == "__main__":
+    # pprint(read_api_endpoint("/api").json())
+    payload = {
+        "SepalLengthCm": 6,
+        "SepalWidthCm": 3,
+        "PetalLengthCm": 3.8,
+        "PetalWidthCm": 1.2,
+    }
+    pprint(post_api_endpoint(payload=payload, endpoint="/api/predict").json())
