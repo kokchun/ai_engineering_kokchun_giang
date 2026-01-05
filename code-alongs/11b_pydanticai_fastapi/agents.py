e3d4606396b1af25b761f0fb9b6016e299dc3c59
kokchun <kokchungiang@gmail.com>
2026-01-05 13:40:55 +0100

    agent setup

---
+++ code-alongs/11b_pydanticai_fastapi/agents.py
@@ -0,0 +1,14 @@
+from pydantic_ai import Agent
+from dotenv import load_dotenv
+from data_models import Movie
+
+load_dotenv()
+
+agent = Agent(
+    model="google-gla:gemini-2.5-flash",
+    system_prompt="""You are a movie expert, based
+              on a prompt, you should find the closest 
+              matching movie
+            """,
+    output_type=Movie,
+)
--- code-alongs/11b_pydanticai_fastapi/api.py
+++ code-alongs/11b_pydanticai_fastapi/api.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 from utils import query_duckdb
 
+
+
 app = FastAPI()
 
 @app.get("/movies")
 async def read_movies():
     movies = query_duckdb("FROM movies;")
     return movies.to_dict(orient="records")
+
