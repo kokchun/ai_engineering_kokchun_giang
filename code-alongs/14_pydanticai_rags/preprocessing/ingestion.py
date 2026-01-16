e8ec697714d4b0dbcb73a8dec1067e95d568dc66
kokchun <kokchungiang@gmail.com>
2026-01-16 11:16:54 +0100

    ingestion

--- code-alongs/14_pydanticai_rags/preprocessing/ingestion.py
+++ code-alongs/14_pydanticai_rags/preprocessing/ingestion.py
@@ -3,6 +3,8 @@ import lancedb
 from lancedb.pydantic import LanceModel, Vector
 from pydantic import Field
 from lancedb.embeddings import get_registry
+from pathlib import Path
+from lancedb.table import LanceTable
 
 embedding_model = get_registry().get("gemini-text").create(name="gemini-embedding-001")
 
@@ -12,3 +14,22 @@ class Article(LanceModel):
     filepath: str 
     content: str = embedding_model.SourceField()
     embedding: Vector(3072) = embedding_model.VectorField()
+
+def setup_vector_db(path):
+    # Path(path).mkdir(exist_ok=True)
+    vector_db = lancedb.connect(path)
+    vector_db.create_table("articles", schema=Article, exist_ok=True)
+
+    return vector_db
+
+def ingest_docs_to_vector_db(table: LanceTable):
+    for filepath in DATA_PATH.glob("*.txt"):
+        with open(filepath, "r") as file:
+            content = file.read()
+
+        doc_id = filepath.stem # or some hash/number
+        table.delete(f"doc_id = '{doc_id}") # make idempotent
+
+        table.add([{
+            
+        }])
